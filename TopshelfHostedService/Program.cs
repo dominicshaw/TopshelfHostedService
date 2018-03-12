c882cd543c8d3d95a0553574c72174181c86ac7c
Dominic Shaw <dominicshaw@hotmail.com>
2018-03-12 12:56:05 +0000

    Initial Commit

---
+++ TopshelfHostedService/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using log4net;
+using Topshelf;
+
+namespace TopshelfHostedService
+{
+    class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            _log.Info("Starting up...");
+
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<TownCrier>(s =>
+                {
+                    s.ConstructUsing(name => new TownCrier());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Sample Topshelf Host");
+                x.SetDisplayName("Stuff");
+                x.SetServiceName("Stuff");
+
+                x.UseLog4Net();
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ TopshelfHostedService/TownCrier.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using log4net;
+
+namespace TopshelfHostedService
+{
+    public class TownCrier
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(TownCrier));
+
+        private readonly object _lock = new object();
+        private readonly Timer _timer;
+
+        public TownCrier()
+        {
+            _timer = new Timer(_ => Cry(), null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void Cry()
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    _log.Info($"It is {DateTime.Now} and all is well");
+                }
+                finally
+                {
+                    _timer.Change(TimeSpan.FromSeconds(1), Timeout.InfiniteTimeSpan);
+                }
+            }
+        }
+
+        public void Start()
+        {
+            lock (_lock)
+            {
+                _timer.Change(TimeSpan.FromSeconds(1), Timeout.InfiniteTimeSpan);
+            }
+        }
+
+        public void Stop()
+        {
+            lock (_lock)
+            {
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+        }
+    }
+}
